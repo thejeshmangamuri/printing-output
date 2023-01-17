c0650a0d4f4e43e3fb55d452923fc487f95879d4
thejeshmangamuri <122370001+thejeshmangamuri@users.noreply.github.com>
2023-01-17 09:28:16 -0500

    JS Code added to the JS file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
+let a = 3;
+let b = 5;
+let c;
 
+let output = 'let a = 3;\n';
+output += 'let b = 5;\n';
+output += 'let c;\n';
+output += '-----------\n';
+output += 'a + b = ' + (a + b) + "\n";
+alert(output);
