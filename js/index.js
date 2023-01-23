b0d72ae4252e8193ace1ef8ec91fa9e0d17c3bae
thejeshmangamuri <122370001+thejeshmangamuri@users.noreply.github.com>
2023-01-23 15:53:51 -0500

    Updated for both the tasks

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// initial setup
 let a = 3;
 let b = 5;
 let c;
@@ -23,4 +24,32 @@ output += 'a < b = ' + (a < b) + "\n";
 output += '!a && !c = ' + (!a && !c) + "\n";
 output += '!a || !c = ' + (!a || !c) + "\n";
 
+alert(output);
+
+// let sum = a+b;
+// let sub = a-b;
+// let mul = a*b;
+
+// alert()
+
+let first_name = "Thejesh";
+let last_name = "Mangamuri";
+let email = "mang0095@algonquinlive.com";
+
+// Single line Chaining
+/*let output;
+output = `My name is ${first_name} ${last_name}. You can contact me at ${email  }.`;
+*/
+
+// Multi line Chaining
+    output = 'My name is '
+    output += first_name;
+    output += ' ';
+    output += last_name;
+    output += '.'
+    output += ' ';
+    output += 'You can contact me at ';
+    output += email;
+    output += '.'
+    
 alert(output);
